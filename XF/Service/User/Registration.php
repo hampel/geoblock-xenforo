e6d1036e3d16b4981729d9be4a97566a424b97eb
Simon Hampel <simon@hampelgroup.com>
2024-03-14 16:35:44 +1100

    slight re-arrange to order of code logic - no point checking anything related to EU blocking if we don't have EU blocking enabled

--- XF/Service/User/Registration.php
+++ XF/Service/User/Registration.php
@@ -10,37 +10,37 @@ class Registration extends XFCP_Registration
 	{
 		parent::checkForSpam();
 
-		$this->geoblockCheckEu();
+        if (RejectEu::isEnabled())
+        {
+            $this->geoblockCheckEu();
+        }
 	}
 
 	public function geoblockCheckEu()
 	{
-		$user = $this->user;
+        $user = $this->user;
 
-		if ($user->user_state == 'rejected')
-		{
-			return; // stop if user already rejected
-		}
+        if ($user->user_state == 'rejected')
+        {
+            return; // stop if user already rejected
+        }
 
-		if (RejectEu::isEnabled())
-		{
-			$ip = \XF\Util\Ip::convertIpStringToBinary(
-				$this->app->request()->getIp(true)
-			);
+        $ip = \XF\Util\Ip::convertIpStringToBinary(
+            $this->app->request()->getIp(true)
+        );
 
-			/** @var Maxmind $api */
-			$api = $this->app->get('geoblock');
+        /** @var Maxmind $api */
+        $api = $this->app->get('geoblock');
 
-			/** @var \Hampel\Geoblock\IpGeo $geo */
-			$geo = $api->getIpGeo($ip);
-			if ($geo && $geo->isInEu() && !Approved::inList($geo->getIsoCode()))
-			{
-				$type = 'Geoblock-EU';
-				$userChecker = $this->app->spam()->userChecker();
-				$userChecker->logDetail($type, 'geoblock_registration_in_eu');
-				$userChecker->logDecision($type, 'denied');
-				$user->rejectUser(\XF::phrase('geoblock_registration_rejected_eu'));
-			}
-		}
+        /** @var \Hampel\Geoblock\IpGeo $geo */
+        $geo = $api->getIpGeo($ip);
+        if ($geo && $geo->isInEu() && !Approved::inList($geo->getIsoCode()))
+        {
+            $type = 'Geoblock-EU';
+            $userChecker = $this->app->spam()->userChecker();
+            $userChecker->logDetail($type, 'geoblock_registration_in_eu');
+            $userChecker->logDecision($type, 'denied');
+            $user->rejectUser(\XF::phrase('geoblock_registration_rejected_eu'));
+        }
 	}
 }
